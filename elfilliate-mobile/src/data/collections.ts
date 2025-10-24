44866ade925409311114d5c9779788531bb002be
John Daly <jcdaly97@gmail.com>
2025-10-23 22:04:08 -0400

    Mobile data: ensure each top-level collection lists 5 products and use real product images

--- elfilliate-mobile/src/data/collections.ts
+++ elfilliate-mobile/src/data/collections.ts
@@ -28,6 +28,12 @@ export const collections = [
         image: require("../../assets/images/product-4.jpg"),
         price: 7.99,
       },
+      {
+        id: 105,
+        name: "Tinted Blush",
+        image: require("../../assets/images/product-6.jpg"),
+        price: 6.49,
+      },
     ],
     engagement: {
       views: 2847,
@@ -64,6 +70,12 @@ export const collections = [
         image: require("../../assets/images/product-2.jpg"),
         price: 12.99,
       },
+      {
+        id: 205,
+        name: "Bite-Size Eyeshadow",
+        image: require("../../assets/images/product-1.jpg"),
+        price: 5.0,
+      },
     ],
     engagement: {
       views: 2847,
@@ -79,26 +91,32 @@ export const collections = [
       {
         id: 301,
         name: "Dewy Highlighter",
-        image: require("../../assets/placeholder-product.png"),
+        image: require("../../assets/images/product-3.jpg"),
         price: 8.99,
       },
       {
         id: 302,
         name: "Illuminating Primer",
-        image: require("../../assets/placeholder-product.png"),
+        image: require("../../assets/images/product-2.jpg"),
         price: 10.99,
       },
       {
         id: 303,
         name: "Glow Drops",
-        image: require("../../assets/placeholder-product.png"),
+        image: require("../../assets/images/product-6.jpg"),
         price: 12.99,
       },
       {
         id: 304,
         name: "Setting Spray",
-        image: require("../../assets/placeholder-product.png"),
+        image: require("../../assets/images/product-7.jpg"),
         price: 9.99,
+      },
+      {
+        id: 305,
+        name: "Halo Glow Filter",
+        image: require("../../assets/images/product-8.jpg"),
+        price: 14.0,
       },
     ],
     engagement: {
